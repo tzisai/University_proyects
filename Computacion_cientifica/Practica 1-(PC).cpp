7357db91666ac9cc40db981a36dfc55e5767513d
Martin_Ryu <raisainu@gmail.com>
2025-02-04 12:08:51 -0600

    practica 1 de programacion Computacion_cientifica en este algoritmo se ve el algoritmmo de  Euclides de manera recursiva y iterativa.

---
+++ Computacion_cientifica/Practica 1-(PC).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+char c;
+bool cond=1;
+
+
+while(cond==1){
+    system("clear");
+
+    switch (c)
+                {
+                case 'r':
+                    
+                break;
+
+                case 'i':
+
+                break;
+
+                default:
+                    break;
+                }
+}
+
+
+
+
+
+return 0;
+}
